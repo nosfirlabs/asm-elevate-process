28d860d1753e0e5ddb8b9bee265171ad292f08f5
NOSFIR! labs <80362772+nosfirlabs@users.noreply.github.com>
2022-12-29 15:11:19 +0100

    Update elevate.asm

--- elevate.asm
+++ elevate.asm
@@ -51,7 +51,3 @@ invoke MessageBoxA, 0, ADDR dwExitCode, ADDR dwExitCode, MB_OK
 invoke ExitProcess, 0
 
 end start
-
-This code first opens the current process token using the OpenProcessToken function and sets it to be a primary token using the SetTokenInformation function. It then creates a new process with the system token using the CreateProcessWithTokenW function, passing the handle to the primary token as an argument. The new process is specified in the lpCommandLine parameter and is launched with the CREATE_NEW_CONSOLE flag to create a new console window.
-
-The main process then waits for the new process to finish using the WaitForSingleObject function, gets the exit code using the GetExitCodeProcess function, and displays it using the MessageBoxA function. Finally, the handle to the new process and the primary token are closed and the main
